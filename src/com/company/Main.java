cb950c6d5ef6ad297af5c287c9c92af8121b6795
mcndoungerii <allenmcndounger@gmail.com>
2021-02-01 23:01:25 +0300

    Java ArrayLists

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,9 +10,22 @@ public class Main {
         float[] floatArray = {3.4f,5.67f,6.98f};
         double[] doubleArray = {23.44,56.78,89.67};
 
-        for (int i = 0; i < doubleArray.length ; i++) {
+        int[] myNums = new int[5];
+        String[] myStrings = new String[3];
 
-            System.out.println("Item "+ doubleArray[i]);
+        myNums[0] = 34;
+        myNums[1] = 43;
+        myNums[2] = 45;
+        myNums[3] = 65;
+        myNums[4] = 76;
+
+        myStrings[0] = "Hello";
+        myStrings[1] = "I'm";
+        myStrings[2] = "Adam";
+
+        for (int i = 0; i < myStrings.length ; i++) {
+
+            System.out.println("Item "+ myStrings[i]);
 
         }
     }
