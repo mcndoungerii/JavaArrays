f148943152df83697198d27efc3a8f5d635cb880
mcndoungerii <allenmcndounger@gmail.com>
2021-02-02 12:52:50 +0300

    HashMaps with Data types

--- src/com/company/Main2.java
+++ src/com/company/Main2.java
@@ -7,6 +7,15 @@ import java.util.Map;
 public class Main2 {
     public static void main(String[] args){
         HashMap users = new HashMap();
+
+        HashMap<String, Integer> newUsers = new HashMap<>();
+        newUsers.put("Adam",21);
+        newUsers.put("Kevin",25);
+        newUsers.put("Lewis",31);
+
+        System.out.println("New users are; " + newUsers.get("Adam"));
+
+        //HashMap input for hashmap users
         users.put("Sandra",23);
         users.put("Adam",31);
         users.put("Sameer",34);
@@ -16,7 +25,7 @@ public class Main2 {
         while (iterator.hasNext()){
             Map.Entry pair = (Map.Entry) iterator.next();
 
-            System.out.println("Key : " + pair.getKey() + " ,"+ " Value :" + pair.getValue());
+            //System.out.println("Key : " + pair.getKey() + " ,"+ " Value :" + pair.getValue());
         }
 
         //System.out.println(users.entrySet());
